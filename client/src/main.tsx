c452b8fe7459729be20a6b8fcca05645ce0cb70d
Hakan Kayra Doğan <hkayrad@gmail.com>
2025-07-24 10:44:04 +0300

    feat: add open-layers map to the client
    Signed-off-by: Hakan Kayra Doğan <hkayrad@gmail.com>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router";
 import './index.css'
-import App from './App.tsx'
+import App from './App';
+import Map from './routes/Map';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/map' element={<Map />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
